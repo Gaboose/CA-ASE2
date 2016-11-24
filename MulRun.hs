12be1f420dc05f3205c525bc270bdc08e3667f28
Gabrielius Mickevičius <gabrielius.m@gmail.com>
2016-11-24 14:17:40 +0000

    Redo MulRun

--- MulRun.hs
+++ MulRun.hs
@@ -1,206 +1,69 @@
-module TestMul where
-import HDL.Hydra.Core.Lib
+------------------------------------------------------------------------
+--  MulRun: machine language program for the Sigma16 architecture
+------------------------------------------------------------------------
+
+{- A machine language program for the Sigma16 architecture that
+performs a number of multiplications with the mul instruction. -}
+
+module Main where
 import M1driver
 
--- Print a line to separate output from different simulations
-separator :: [Char] -> IO()
-separator prog_name =
-  do
-    putStrLn (take 90 (repeat '-'))
-    putStrLn ("Starting new test")
-    putStrLn ("Test name: " ++ prog_name)
-    putStrLn (take 90 (repeat '-'))
-
 main :: IO ()
-main =
-  do
-    separator "test_small_positive_numbers"
-    run_Sigma16_program test_small_positive_numbers 200
-
-    separator "test_small_negative_numbers"
-    run_Sigma16_program test_small_negative_numbers 200
-
-    separator "test_big_positive_numbers"
-    run_Sigma16_program test_big_positive_numbers 200
-
-    separator "test_positive_negative_numbers"
-    run_Sigma16_program test_positive_negative_numbers 200
-
-    separator "test_positive_negative_numbers2"
-    run_Sigma16_program test_positive_negative_numbers2 200
-
-    separator "test_big_positive_numbers_overflow"
-    run_Sigma16_program test_big_positive_numbers_overflow 200
-
-    separator "test_big_negative_numbers_overflow"
-    run_Sigma16_program test_big_negative_numbers_overflow 200
-
-
+main = run_Sigma16_program multest 10000
 
 ------------------------------------------------------------------------
 
--- test for small positive numbers
-test_small_positive_numbers :: [String]
-test_small_positive_numbers =
+multest :: [String]
+multest =
 -- Machine Language  Addr    Assembly Language     Comment
 -- ~~~~~~~~~~~~~~~~  ~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  [
-                   -- 0000 ; Initialise
-                   -- 0000
-  "f100", "0002",  -- 0000 start lea R1,2[R0]       ; R1 = constant 2
-  "f200", "0004",  -- 0002       lea R2,2[R0]       ; R2 = constant 2
-  "2321",          -- 0004       mul R3,R2,R1       ; R3 = R2*R1
-  "f400", "0008",  -- 0005       lea R4,4[R0]       ; R4 = constant 4
+                    -- 0000 ; Mul: multiply several pairs of numbers and
+                    -- 0000 ; save the results into registers 3 to 7.
+                    -- 0000
+                    -- 0000 ; Register usage
+                    -- 0000 ; R1, R2 - multiplication factors
+                    -- 0000 ; R3, R4, R5, R6, R7 - products for 5 mul operations
+                    -- 0000
+                    -- 0000
+                    -- 0000 ; Multiply two small numbers
+    "f100", "0002", -- 0000 lea R1,2[R0]     ; R1 = 2
+    "f200", "0004", -- 0002 lea R2,4[R0]     ; R2 = 4
+    "2321",         -- 0004 mul R3,R2,R1     ; R3 = 2*4 = 8        (0x0008)
+                    -- 0005
+                    -- 0005 ; Multiply a positive number with a negative number
+    "f100", "0003", -- 0005 lea R1,3[R0]     ; R1 = 3
+    "f200", "ffff", -- 0007 lea R2,ffff[R0]  ; R2 = -1
+    "2421",         -- 0009 mul R4,R2,R1     ; R4 = 3*(-1) = -3    (0xfffd)
+                    -- 000a
+                    -- 000a ; Multiply two negative numbers
+    "f100", "fffb", -- 000a lea R1,fffb[R0]  ; R1 = -5
+    "f200", "ffff", -- 000c lea R2,ffff[R0]  ; R2 = -1
+    "2521",         -- 000e mul R5,R2,R1     ; R5 = (-5)*(-1) = 5  (0x0005)
+                    -- 000f
+                    -- 000f ; Multiply two relatively large numbers
+    "f100", "007f", -- 000f lea R1,7f[R0]    ; R1 = 127
+    "f200", "0101", -- 0011 lea R2,101[R0]   ; R2 = 257
+    "2621",         -- 0013 mul R6,R2,R1     ; R6 = 127 * 257 = 32639 (in dec)
+                    -- 0014                  ;       7f * 101 =  7f7f (in hex)
+                    -- 0014
+                    -- 0014 ; Multiply two numbers that are too large
+    "f100", "00ff", -- 0014 lea R1,ff[R0]    ; R1 = 255
+    "f200", "1001", -- 0016 lea R2,1001[R0]  ; R2 = 4097
+    "2721",         -- 0018 mul R7,R2,R1     ; R7 = 255 * 4097 % 65536  = 1044735 % 65536 = 61695 (in dec)
+                    -- 0019                  ;                              61695 - 65536 =  -384 (in 2s-complement)
+                    -- 0019                  ;       ff * 1001 % 10000  =   ff0ff % 10000 =  f0ff (in hex)
+                    -- 0019                  ; The product overflows here.
+                    -- 0019                  ; The effect is as if we used a modulus 0x10000 operation.
+                    -- 0019
+                    -- 0019 ; A table of expected multiplication products for reference:
+                    -- 0019 ; R3 = 0008
+                    -- 0019 ; R4 = fffd
+                    -- 0019 ; R5 = 0005
+                    -- 0019 ; R6 = 7f7f
+                    -- 0019 ; R7 = f0ff
+                    -- 0019
+    "d000"          -- 0019 trap
+ ]
 
-                   -- 0007 loop if (r3!=r4)
-  "5534",          -- 0007       cmpeq R5,R3,R4     ; R5 = R3 == 4
-  "f504", "0007",  -- 0008 then  jumpf R5,loop[R0]  ; if r3!=r4 goto loop
-  "d000"           -- 000a       trap  R0,R0,R0     ; else terminate
-   ]
-
-------------------------------------------------------------------------
-
--- test for negative numbers
-test_small_negative_numbers :: [String]
-test_small_negative_numbers =
--- Machine Language  Addr    Assembly Language     Comment
--- ~~~~~~~~~~~~~~~~  ~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- [
-                   -- 0000 ; Initialise
-                   -- 0000
-  "f100", "fffe",  -- 0000 start lea R1,fffe[R0]    ; R1 = constant -2
-  "f200", "fffe",  -- 0002       lea R2,fffe[R0]    ; R2 = constant -2
-  "2321",          -- 0004       mul R3,R2,R1       ; R3 = R2*R1
-  "f400", "0004",  -- 0005       lea R4,4[R0]       ; R4 = constant 4
-
-                   -- 0007 loop if (r3!=r4)
-  "5534",          -- 0007       cmpeq R5,R3,R4     ; R5 = R3 == 4
-  "f504", "0007",  -- 0008 then  jumpf R5,loop[R0]  ; if r3!=r4 goto loop
-  "d000"           -- 000a       trap  R0,R0,R0     ; else terminate
-   ]
-
-
-------------------------------------------------------------------------
-
--- test for big positive numbers
-test_big_positive_numbers :: [String]
-test_big_positive_numbers =
--- Machine Language  Addr    Assembly Language     Comment
--- ~~~~~~~~~~~~~~~~  ~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- [
-                   -- 0000 ; Initialise
-                   -- 0000
-  "f100", "0096",  -- 0000 start lea R1,96[R0]      ; R1 = constant 150
-  "f200", "0064",  -- 0002       lea R2,64[R0]      ; R2 = constant 100
-  "2321",          -- 0004       mul R3,R2,R1       ; R3 = R2*R1
-  "f400", "3a98",  -- 0005       lea R4,3a98[R0]    ; R4 = constant 15000
-
-                   -- 0007 loop if (r3!=r4)
-  "5534",          -- 0007       cmpeq R5,R3,R4     ; R5 = R3 == 15000
-  "f504", "0007",  -- 0008 then  jumpf R5,loop[R0]  ; if r3!=r4 goto loop
-  "d000"           -- 000a       trap  R0,R0,R0     ; else terminate
-   ]
-
-
-------------------------------------------------------------------------
-
--- test for negative * positive numbers
-test_positive_negative_numbers :: [String]
-test_positive_negative_numbers =
--- Machine Language  Addr    Assembly Language     Comment
--- ~~~~~~~~~~~~~~~~  ~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- [
-                   -- 0000 ; Initialise
-                   -- 0000
-  "f100", "fffe",  -- 0000 start lea R1,fffe[R0]    ; R1 = constant -2
-  "f200", "0002",  -- 0002       lea R2,2[R0]       ; R2 = constant  2
-  "2321",          -- 0004       mul R3,R2,R1       ; R3 = R2*R1
-  "f400", "fffc",  -- 0005       lea R4,fffc[R0]    ; R4 = constant -4
-
-                   -- 0007 loop if (r3!=r4)
-  "5534",          -- 0007       cmpeq R5,R3,R4     ; R5 = R3 == -4
-  "f504", "0007",  -- 0008 then  jumpf R5,loop[R0]  ; if r3!=r4 goto loop
-  "d000"           -- 000a       trap  R0,R0,R0     ; else terminate
-   ]
-
-
-------------------------------------------------------------------------
-
--- test for negative * positive numbers2
-test_positive_negative_numbers2 :: [String]
-test_positive_negative_numbers2 =
--- Machine Language  Addr    Assembly Language     Comment
--- ~~~~~~~~~~~~~~~~  ~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- [
-                   -- 0000 ; Initialise
-                   -- 0000
-  "f100", "fffe",  -- 0000 start lea R1,fffe[R0]    ; R1 = constant -2
-  "f200", "0006",  -- 0002       lea R2,6[R0]       ; R2 = constant  6
-  "2321",          -- 0004       mul R3,R2,R1       ; R3 = R2*R1
-  "f400", "fff4",  -- 0005       lea R4,fff4[R0]    ; R4 = constant -12
-
-                   -- 0007 loop if (r3!=r4)
-  "5534",          -- 0007       cmpeq R5,R3,R4     ; R5 = R3 == -12
-  "f504", "0007",  -- 0008 then  jumpf R5,loop[R0]  ; if r3!=r4 goto loop
-  "d000"           -- 000a       trap  R0,R0,R0     ; else terminate
-   ]
-
-
-
-------------------------------------------------------------------------
-
--- test for big positive numbers
--- NOTE:
--- this test shows how my implementation overflows. This happens because the
--- multiplier takes 2 16bits long words and produces a product which is 32bits
--- wide. However our registry holds only 16bit words so on when saving the result
--- we only take the 16 least significant bits from the product and thus the overflow
-
-
-test_big_positive_numbers_overflow :: [String]
-test_big_positive_numbers_overflow =
--- Machine Language  Addr    Assembly Language     Comment
--- ~~~~~~~~~~~~~~~~  ~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- [
-                   -- 0000 ; Initialise
-                   -- 0000
-  "f100", "8000",  -- 0000 start lea R1,8000[R0]    ; R1 = constant 32768
-  "f200", "0002",  -- 0002       lea R2,2[R0]       ; R2 = constant 2
-  "2321",          -- 0004       mul R3,R2,R1       ; R3 = R2*R1
-  "f400", "0000",  -- 0005       lea R4,0[R0]       ; R4 = constant 0 (and the real result should be f0000)
-
-                   -- 0007 loop if (r3!=r4)
-  "5534",          -- 0007       cmpeq R5,R3,R4     ; R5 = R3 == 15000
-  "f504", "0007",  -- 0008 then  jumpf R5,loop[R0]  ; if r3!=r4 goto loop
-  "d000"           -- 000a       trap  R0,R0,R0     ; else terminate
-   ]
-
-
-------------------------------------------------------------------------
-
--- test for big negative numbers
--- NOTE:
--- this test shows how my implementation overflows. This happens because the
--- multiplier takes 2 16bits long words and produces a product which is 32bits
--- wide. However our registry holds only 16bit words so on when saving the result
--- we only take the 16 least significant bits from the product and thus the overflow
-
-
-test_big_negative_numbers_overflow :: [String]
-test_big_negative_numbers_overflow =
--- Machine Language  Addr    Assembly Language     Comment
--- ~~~~~~~~~~~~~~~~  ~~~~ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- [
-                   -- 0000 ; Initialise
-                   -- 0000
-  "f100", "8001",  -- 0000 start lea R1,8001[R0]    ; R1 = constant -32767
-  "f200", "0002",  -- 0002       lea R2,2[R0]       ; R2 = constant 2
-  "2321",          -- 0004       mul R3,R2,R1       ; R3 = R2*R1
-  "f400", "0002",  -- 0005       lea R4,2[R0]       ; R4 = constant 2 (should be -65534)
-
-                   -- 0007 loop if (r3!=r4)
-  "5534",          -- 0007       cmpeq R5,R3,R4     ; R5 = R3 == 2
-  "f504", "0007",  -- 0008 then  jumpf R5,loop[R0]  ; if r3!=r4 goto loop
-  "d000"           -- 000a       trap  R0,R0,R0     ; else terminate
-   ]
-
+ ------------------------------------------------------------------------
